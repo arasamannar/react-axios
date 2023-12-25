df3072292272af9643e7f2664009bd7b750911a5
Arasamannar A K <arasamannar@gmail.com>
2023-12-25 20:16:02 +0530

    Made changes to the whole data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import ListPage from "./Components/ListPage";
-import { UserContextProvider } from "./Components/usercontext";
-import './App.css';
+// App.js
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import UserReducer, { setUsers } from './UserReducer';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Home from './Home';
+import Create from './Create';
+import Update from './Update';
 
+const store = configureStore({
+  reducer: {
+    users: UserReducer,
+  },
+});
 
-function App() {
+const App = () => {
+  useEffect(() => {
+   
+    
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        store.dispatch(setUsers(response.data));
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <UserContextProvider>
+    <Provider store={store}>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/ListPages" element={<ListPage />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/edit/:id" element={<Update />} />
         </Routes>
-      </UserContextProvider>
-    </div>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
